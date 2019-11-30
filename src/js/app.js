61e7a8de2da993194d719b7c6e1c8e7da4e91223
Nathan Silva Santos <nathan@w16.com.br>
2019-11-29 23:50:23 -0300

    MVP

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,59 @@
-const app = {
-    main: document.querySelector('main'),
-    aside: {
-        element: document.querySelector('aside'),
-        btToggle: document.querySelector('aside .bt-toggle'),
-        IsOpen: true
-    },
-
-    toggleAside(aside) {
-        aside.IsOpen
-            ? aside.element.style.width = '50px'
-            : aside.element.style.width = '200px'
-        aside.IsOpen = !aside.IsOpen
-    },
-
-    bindEvents() {
-        this.aside.btToggle.addEventListener('click', () => app.toggleAside(this.aside))
-
-    },
+//somente teste\/
+$(".hidden-images__content").append($('.col:hidden .ux-logo-image').clone())
+//somente teste/\
 
 
-    init() {
-        this.bindEvents()
+var randomizeImagesInitialized = false
+
+var randomizeImages = function () {
+    var TRANSITION_TIME = 1000
+
+    var fadeIn = function (element) {
+        element.style.display = "block"
+        setTimeout(function () { element.style.opacity = 1 }, TRANSITION_TIME)
     }
+
+    var fadeOut = function (element) {
+        element.style.opacity = 0
+        setTimeout(function () { element.style.display = "none" }, TRANSITION_TIME)
+    }
+
+    setInterval(function () {
+        var images = $('.col:visible .ux-logo-image')
+        var hiddenImages = $('.col:hidden .ux-logo-image')
+        var randomImagesLength = parseInt(Math.random() * images.length)
+        var randomHiddenImagesLength = parseInt(Math.random() * hiddenImages.length)
+
+        var currentImageSrc = images[randomImagesLength].getAttribute("src")
+
+        fadeOut(images[randomImagesLength])
+
+        setTimeout(function () {
+
+            images[randomImagesLength].setAttribute('src', hiddenImages[randomHiddenImagesLength].getAttribute('src'))
+            
+            setTimeout(function () {
+            
+                fadeIn(images[randomImagesLength])
+                hiddenImages[randomHiddenImagesLength].setAttribute("src", currentImageSrc)
+                
+                //somente teste\/
+                $(".hidden-images__content").html("")
+                $(".hidden-images__content").append($('.col:hidden .ux-logo-image').clone())
+                //somente teste/\
+
+            }, TRANSITION_TIME / 2)
+
+        }, TRANSITION_TIME)
+    
+    }, 5 * TRANSITION_TIME)
 }
 
-app.init()
+var setRandomizeImagesInterval =  setInterval(function () {
+    if (document.querySelector(".clients-wrapper") != undefined && $('.col:hidden .ux-logo-image').length && !randomizeImagesInitialized) {
+        randomizeImages()
+        randomizeImagesInitialized = true
+        clearInterval(setRandomizeImagesInterval)
+        console.log("randomizeImages initialized")
+    }
+}, 1000)
